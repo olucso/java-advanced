38aed360f99ed73150dafe41adccea1f45f3d8e2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-24 22:58:57 -0300

    primeira aula

---
+++ src/main/java/br/com/fiap/anotation/Coluna.java
@@ -0,0 +1,16 @@
+package br.com.fiap.anotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//Define até quando a anotação estará presente (código fonte
+@Retention(RetentionPolicy.RUNTIME)
+//Define onde pode ser utilizada a anotação
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface Coluna {
+    String nome();
+    int tamanho() default 0;
+    boolean obrigatorio() default false;
+}
